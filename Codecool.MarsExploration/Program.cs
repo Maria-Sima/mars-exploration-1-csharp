7059f111b414413c1ad7f381315ad717fc5b46e8
Maria <simam9520@gmail.com>
2023-03-28 13:44:50 +0300

    merge

--- Codecool.MarsExploration/Program.cs
+++ Codecool.MarsExploration/Program.cs
@@ -1,101 +1,74 @@
-﻿// using Codecool.MarsExploration.Calculators.Service;
-// using Codecool.MarsExploration.Configuration.Model;
-// using Codecool.MarsExploration.Configuration.Service;
-// using Codecool.MarsExploration.MapElements.Service.Builder;
-// using Codecool.MarsExploration.MapElements.Service.Generator;
-// using Codecool.MarsExploration.MapElements.Service.Placer;
-// using Codecool.MarsExploration.Output.Service;
-//
-// internal class Program
-// {
-//     //You can change this to any directory you like
-//     private static readonly string WorkDir = AppDomain.CurrentDomain.BaseDirectory;
-//
-//     public static void Main(string[] args)
-//     {
-//         Console.WriteLine("Mars Exploration Sprint 1");
-//         var mapConfig = GetConfiguration();
-//
-//         IDimensionCalculator dimensionCalculator = null;
-//         ICoordinateCalculator coordinateCalculator = null;
-//
-//         IMapElementBuilder mapElementFactory = null;
-//         IMapElementsGenerator mapElementsGenerator = null;
-//
-//         IMapConfigurationValidator mapConfigValidator = null;
-//         IMapElementPlacer mapElementPlacer = null;
-//
-//         IMapGenerator mapGenerator = null;
-//
-//         CreateAndWriteMaps(3, mapGenerator, mapConfig);
-//
-//         Console.WriteLine("Mars maps successfully generated.");
-//         Console.ReadKey();
-//     }
-//
-//     private static void CreateAndWriteMaps(int count, IMapGenerator mapGenerator, MapConfiguration mapConfig)
-//     {
-//     }
-//
-//     private static MapConfiguration GetConfiguration()
-//     {
-//         const string mountainSymbol = "#";
-//         const string pitSymbol = "&";
-//         const string mineralSymbol = "%";
-//         const string waterSymbol = "*";
-//
-//         var mountainsCfg = new MapElementConfiguration(mountainSymbol, "mountain", new[]
-//         {
-//             new ElementToDimension(2, 20),
-//             new ElementToDimension(1, 30),
-//         }, 3);
-//         
-//         var pitsCfg = new MapElementConfiguration(pitSymbol, "pit", new[]
-//         {
-//             new ElementToDimension(3, 20),
-//             new ElementToDimension(1, 15),
-//         }, 10);
-//         
-//         var mineralsCfg = new MapElementConfiguration(mineralSymbol, "mineral", new[]
-//         {
-//             new ElementToDimension(5, 1),
-//         }, 0);
-//         
-//         var watersCfg = new MapElementConfiguration(waterSymbol, "water", new[]
-//         {
-//             new ElementToDimension(6, 1),
-//         }, 0);
-//
-//         List<MapElementConfiguration> elementsCfg = new() { mountainsCfg, pitsCfg, mineralsCfg, watersCfg };
-//         return new MapConfiguration(1000, 0.5, elementsCfg);
-//     }
-// {
- static string CleanString(string s)
+﻿using Codecool.MarsExploration.Calculators.Service;
+using Codecool.MarsExploration.Configuration.Model;
+using Codecool.MarsExploration.Configuration.Service;
+using Codecool.MarsExploration.MapElements.Service.Builder;
+using Codecool.MarsExploration.MapElements.Service.Generator;
+using Codecool.MarsExploration.MapElements.Service.Placer;
+using Codecool.MarsExploration.Output.Service;
+
+internal class Program
 {
-        
-   
+    //You can change this to any directory you like
+    private static readonly string WorkDir = AppDomain.CurrentDomain.BaseDirectory;
 
-        while (s.Length>0)
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Mars Exploration Sprint 1");
+        var mapConfig = GetConfiguration();
+
+        IDimensionCalculator dimensionCalculator = null;
+        ICoordinateCalculator coordinateCalculator = null;
+
+        IMapElementBuilder mapElementFactory = null;
+        IMapElementsGenerator mapElementsGenerator = null;
+
+        IMapConfigurationValidator mapConfigValidator = null;
+        IMapElementPlacer mapElementPlacer = null;
+
+        IMapGenerator mapGenerator = null;
+
+        CreateAndWriteMaps(3, mapGenerator, mapConfig);
+
+        Console.WriteLine("Mars maps successfully generated.");
+        Console.ReadKey();
+    }
+
+    private static void CreateAndWriteMaps(int count, IMapGenerator mapGenerator, MapConfiguration mapConfig)
+    {
+    }
+
+    private static MapConfiguration GetConfiguration()
+    {
+        const string mountainSymbol = "#";
+        const string pitSymbol = "&";
+        const string mineralSymbol = "%";
+        const string waterSymbol = "*";
+
+        var mountainsCfg = new MapElementConfiguration(mountainSymbol, "mountain", new[]
         {
-           int i = 0;
-            if (s[i] == '#' && i!=0)
-            {
-                s[i] = s[i++];
-                i++;
-            }
-            else  
-            {
-                s[i] = s[i++];
-                i++;
-            }
-            
+            new ElementToDimension(2, 20),
+            new ElementToDimension(1, 30),
+        }, 3);
+        
+        var pitsCfg = new MapElementConfiguration(pitSymbol, "pit", new[]
+        {
+            new ElementToDimension(3, 20),
+            new ElementToDimension(1, 15),
+        }, 10);
+        
+        var mineralsCfg = new MapElementConfiguration(mineralSymbol, "mineral", new[]
+        {
+            new ElementToDimension(5, 1),
+        }, 0);
+        
+        var watersCfg = new MapElementConfiguration(waterSymbol, "water", new[]
+        {
+            new ElementToDimension(6, 1),
+        }, 0);
 
-        }
-    
-    return s;
-}
+        List<MapElementConfiguration> elementsCfg = new() { mountainsCfg, pitsCfg, mineralsCfg, watersCfg };
+        return new MapConfiguration(1000, 0.5, elementsCfg);
+    }
 
-
-     Console.WriteLine(CleanString("abc#d##c"));
-     ;
+  
 
